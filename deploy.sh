b37ccb71b8400753c2f97a70e6add63112e8bd09
Joan Domingo <joands@gmail.com>
2021-03-10 12:33:56 +0100

    Add script to deploy on ec2

---
+++ deploy.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+rm -rf build/
+echo "delete build folder"
+
+./gradlew build
+echo "app built"
+
+./gradlew bootJar
+echo "jar file generated"
+
+scp -r -i kafka-demo-ec2-instance.pem /build/libs/kafka-demo-0.0.1-SNAPSHOT.jar ec2-user@18.185.102.47:/home/ec2-user
+echo "jar file copied to ec2 instance"
+
+echo "Connecting to ec2 instance"
+ssh -i kafka-demo-ec2-instance.pem ec2-user@18.185.102.47 sudo fuser -k 8080/tcp
+echo "Killed process running on port 8080"
+
+ssh -i kafka-demo-ec2-instance.pem ec2-user@18.185.102.47 java -jar kafka-demo-0.0.1-SNAPSHOT.jar
+echo "Started server using java -jar command"
